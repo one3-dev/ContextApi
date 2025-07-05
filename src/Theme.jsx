d2533284b196c75290f68be1061aba42e32c1d9d
Dev Gandhi <90267101+one3-dev@users.noreply.github.com>
2025-07-05 15:00:43 +0530

    Update Theme.jsx

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -3,14 +3,13 @@ import React, { createContext, useState } from 'react';
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-  // Initialize the theme from localStorage or default to 'light'
   const savedTheme = localStorage.getItem('theme') || 'light';
   const [theme, setTheme] = useState(savedTheme);
 
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
-    localStorage.setItem('theme', newTheme); // Persist the theme in localStorage
+    localStorage.setItem('theme', newTheme); 
   };
 
   return (
