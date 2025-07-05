af307bf0c9dd3a1dd15d079e18c184d6fad398cf
Dev Gandhi <90267101+one3-dev@users.noreply.github.com>
2025-07-05 15:01:09 +0530

    Update Language.jsx

--- src/Language.jsx
+++ src/Language.jsx
@@ -3,14 +3,13 @@ import React, { createContext, useState } from 'react';
 const LanguageContext = createContext();
 
 const LanguageProvider = ({ children }) => {
-  // Initialize language from localStorage or default to 'en'
   const savedLanguage = localStorage.getItem('language') || 'en';
   const [language, setLanguage] = useState(savedLanguage);
 
   const toggleLanguage = () => {
     const newLanguage = language === 'en' ? 'es' : 'en';
     setLanguage(newLanguage);
-    localStorage.setItem('language', newLanguage); // Persist the language in localStorage
+    localStorage.setItem('language', newLanguage); 
   };
 
   return (
